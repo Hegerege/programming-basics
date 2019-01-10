0cbca747aca5187be107f22005d3a277bbb61db3
Heikki Honkanen <heikki.honkanen@student.saimia.fi>
2019-01-10 20:11:00 +0200

    if-task completed

---
+++ if-task1/if-task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace if_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Käyttäjää pyydetään kirjoittamaan ohjelmalle 1 luku. Ohjelma tulostaa positiivinen, negatiivinen tai nolla");
+           
+            Console.WriteLine("Anna joku luku");
+            string userInput = Console.ReadLine();
+            int muuttuja = int.Parse(userInput);
+
+            if (muuttuja == 0)
+                Console.WriteLine("Tulos on nolla");
+            else if (muuttuja < 0)
+                Console.WriteLine("Tulos on negatiivinen");
+            else
+                Console.WriteLine("Tulos on positiivinen");
+          
+
+         
+         
+
+        }
+    }
+}
